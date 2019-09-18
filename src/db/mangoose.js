a1c2b0e688a19ce9fb3cca4c9334404b1ee0d343
Badal <badal@turningcloud.com>
2019-09-18 12:50:59 +0530

    url

--- src/db/mangoose.js
+++ src/db/mangoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb:mongodb+srv://taskapp:goldmetal@cluster0-eb281.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true ,
     useCreateIndex :true ,
     // useFindAndModify : true
